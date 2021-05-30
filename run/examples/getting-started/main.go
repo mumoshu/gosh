ae0cd0908e849ecb389d78b0f82996f6cbf7c400
Yusuke Kuoka <ykuoka@gmail.com>
2021-05-30 05:25:05 +0000

    Fix gitignore and add getting-started

---
+++ run/examples/getting-started/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"os"
+
+	"github.com/mumoshu/gosh"
+)
+
+func main() {
+	sh := &gosh.Shell{}
+
+	sh.Def("hello", func(ctx gosh.Context, target string) {
+		// sh.Diagf("My own debug message someData=%s someNumber=%d", "foobar", 123)
+
+		ctx.Stdout().Write([]byte("hello " + target + "\n"))
+	})
+
+	sh.MustExec(os.Args)
+}
